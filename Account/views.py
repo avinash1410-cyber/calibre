f68b0184c599268d040b07bd5fe49664a740ab32
Avinash Kumar <avi8654340@gmail.com>
2022-08-07 17:56:39 +0530

    sycog2

--- Account/views.py
+++ Account/views.py
@@ -1,11 +1,8 @@
-from http.client import HTTPResponse
 from .models import Customer
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate,login
-from rest_framework_jwt.views import obtain_jwt_token
 
 @api_view(('GET','POST'))
 def register_page(request):
